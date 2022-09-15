e7faf3e8fecdb94532b31007f8bf7f3e3327ae75
Oplp25 <jakobwood25@gmail.com>
2022-09-15 11:10:21 +0100

    added movement and drawing ogf the animals

--- Evolution Sim.py
+++ Evolution Sim.py
@@ -5,7 +5,6 @@ pygame.init()
 
 win=pygame.display.set_mode((900,900))
 brown=(181,101,29)
-win.fill(brown)
 pygame.display.update()
 
 def getColour():
@@ -49,9 +48,20 @@ class animal(object):
         self.y=y
         self.species=attributes[8]
         self.age=0
-    def move(self):
-        pass
     
+    def draw(self):
+        pygame.draw.rect(win,self.colour,(self.x*9+1,self.y*9+1,8,8))
+    def move(self):
+        for i in range(self.speed):
+            x=randint(1,4)
+            if x==1:
+                self.x+=1
+            elif x==2:
+                self.x-=1
+            elif x==3:
+                self.y+=1
+            else:
+                self.y-=1
     def eat(self):
         pass
     
@@ -70,21 +80,32 @@ class animal(object):
     def checkIfDie(self):
         pass
 
-def drawGrid():  
-    for x,y,z in zip(range(100),range(100),range(100)):
+def drawGrid():
+    win.fill(brown)
+    for x,y in zip(range(100),range(100)):
         pygame.draw.rect(win,(0,0,0),(x*9,1,1,899))
-        pygame.display.update()
         pygame.draw.rect(win,(0,0,0),(1,y*9,899,1))
 drawGrid()
 pygame.display.update()
 speciesList=[species(1,4,2,0,1,1,6,False,(210,180,140)),species(3,3,6,3,3,3,24,True,(255,128,0))]
 animalList=[]
+clock=pygame.time.Clock()
+fps=5
 for i in range(25):
-    x=speciesList[0].callFunction()
     animalList.append(animal(speciesList[0].callFunction(),randint(0,100),randint(0,100)))
-    print(animalList[-1].x,animalList[-1].y)
+for i in range(4):
+    animalList.append(animal(speciesList[1].callFunction(),randint(0,100),randint(0,100)))
+def update():
+    drawGrid()
+    for i in animalList:
+        i.move()
+        i.draw()
+    pygame.display.update()
 while True:
+    clock.tick(fps)
+    deltatime=clock.tick(60)/1024
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
             exit()
+    update()
