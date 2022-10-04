1fc9e5c937fe1935844d1cf130035bb8b9ce318a
Oplp25 <jakobwood25@gmail.com>
2022-10-04 15:05:51 +0100

    Update Evolution Sim.py

--- Evolution Sim.py
+++ Evolution Sim.py
@@ -51,6 +51,7 @@ class animal(object):
         self.saturation=attributes[5]
         self.lifespan=attributes[6]
         self.canEatMeat=attributes[8]
+        self.breedingInterval=attributes[7]
         self.x=x
         self.y=y
         self.species=attributes[9]
@@ -59,11 +60,60 @@ class animal(object):
     def draw(self):
         pygame.draw.rect(win,self.colour,(self.x*9+1,self.y*9+1,8,8))
     def move(self):
-        if self.canEatMeat:
+        if not self.wantBreed:
+            if self.canEatMeat:
+                target=animalList[0]
+                targetDist=abs(self.x-animalList[0].x)+abs(self.y-animalList[0].y)
+                for i in animalList:
+                    if i.species in self.species.killList:
+                        if abs(self.x-i.x+self.y-i.y)<targetDist:
+                            target=i
+                            targetDist=abs(self.x-i.x)+abs(self.y-i.y)
+                tick=0
+                for i in range(self.speed):
+                    if tick==0 and self.x!=target.x or tick==1 and self.y==target.y:
+                        if self.x<=target.x:
+                            self.x+=1
+                        elif self.x>=target.x:
+                            self.x-=1
+                        tick=1
+                    elif tick==1 and self.y!=target.y or tick==0 and self.x==target.x:
+                        if self.y<=target.y:
+                            self.y+=1
+                        elif self.y>=target.y:
+                            self.y-=1
+                        tick=0
+
+
+
+            else:
+                for i in range(self.speed):
+                    x=randint(1,4)
+                    if x==1:
+                        if self.x+1>=90:
+                            pass
+                        else:
+                            self.x+=1
+                    elif x==2:
+                        if self.x-1<0:
+                            pass
+                        else:
+                            self.x-=1
+                    elif x==3:
+                        if self.y+1>=90:
+                            pass
+                        else:
+                            self.y+=1
+                    else:
+                        if self.y-1<0:
+                            pass
+                        else:
+                            self.y-=1
+        else:
             target=animalList[0]
             targetDist=abs(self.x-animalList[0].x)+abs(self.y-animalList[0].y)
             for i in animalList:
-                if i.species in self.species.killList:
+                if i.species ==self.species:
                     if abs(self.x-i.x+self.y-i.y)<targetDist:
                         target=i
                         targetDist=abs(self.x-i.x)+abs(self.y-i.y)
@@ -81,32 +131,6 @@ class animal(object):
                     elif self.y>=target.y:
                         self.y-=1
                     tick=0
-
-
-
-        else:
-            for i in range(self.speed):
-                x=randint(1,4)
-                if x==1:
-                    if self.x+1>=90:
-                        pass
-                    else:
-                        self.x+=1
-                elif x==2:
-                    if self.x-1<0:
-                        pass
-                    else:
-                        self.x-=1
-                elif x==3:
-                    if self.y+1>=90:
-                        pass
-                    else:
-                        self.y+=1
-                else:
-                    if self.y-1<0:
-                        pass
-                    else:
-                        self.y-=1
     def checkEat(self):
         if self.canEatMeat:
             for i in animalList:
@@ -131,7 +155,16 @@ class animal(object):
                 
     def checkIfDie(self):
         pass
-
+    def update(self):
+        self.move()
+        self.draw()
+        self.checkEat()
+        if self.sinceLastOffspring>=self.breedingInterval:
+            self.wantBreed=True
+            for i in animalList:
+                if i.species==self.species and self.x==i.x and self.y==i.y:
+                    self.breed(i)#
+                    break
 def drawGrid():
     win.fill(brown)
     for x,y in zip(range(100),range(100)):
@@ -139,6 +172,7 @@ def drawGrid():
         pygame.draw.rect(win,(0,0,0),(1,y*9,899,1))
 drawGrid()
 pygame.display.update()
+#size,speed,maxhp,attack,defense,saturation,lifespan,breedingInterval,canEatMeat,colour=0
 speciesList=[species(1,4,2,0,1,1,0,6,False,(210,180,140)),species(3,3,6,3,3,3,0,24,True,(255,128,0))]
 for i in speciesList:
     if i.canEatMeat:
@@ -153,9 +187,7 @@ for i in range(4):
 def update():
     drawGrid()
     for i in animalList:
-        i.move()
-        i.checkEat()
-        i.draw()
+        i.update()
     pygame.display.update()
 while True:
     clock.tick(fps)
